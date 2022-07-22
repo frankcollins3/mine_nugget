9e313c599d03d75ebd06dd5214c9d351a4192b29
Frank Collins <fwc3rd@gmail.com>
2022-07-21 22:21:00 -0400

    [oreore.ejs] Ore or Ore
    Was finishing up before pushing to production/deployment and realized:
    
    There could be a game where we:
    access the health-benefits/db.effect.gold
    
    create a [1/10 || 2/10] chance upon res.render("/index") or res.render("/strain") [or maybe store in the navbar]:
    a strain/oreore.ejs file that asks if you're more into:
    
    appetite v.s. euphoria
    trouble sleeping v.s. headaches
    
    and will turn around and advise:
    
    "So-&-So Strain"/ apiData would be GOLD for you!
    
    it's another simple endpoint based game (like familytree.ejs)
    that is based on some of the table data being unshown to the user:
    
    ****
    1) family tree is based on the strain.parents not being shown to you on [strain.ejs] or in the search query.
    2) there is also: db.strain.hasMany(db.effect) ---> db.effect.gold ----> [goodFor] 'headaches, tiredness'
    
    anticipating .split(', ') [white space between 'headaches, 'tiredness' etc     
    ****

---
+++ views/oreore.js
@@ -0,0 +1,14 @@
+
+        <h1> Ore or Ore</h1>
+  
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script>
+            $('body')
+            .css('background-color', 'rgb(62, 50, 32)')
+
+
+            $("h1")
+            .css('text-align', 'center')
+            .css('color', 'papayawhip')
+            .css('margin-top', '2.25em')
+        </script>
