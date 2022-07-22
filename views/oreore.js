b42777c47f5a09efc72d819645c0c11dcc57205a
Frank Collins <fwc3rd@gmail.com>
2022-07-21 23:32:41 -0400

    [oreore.ejs] append randomKeyword to container

--- views/oreore.js
+++ views/oreore.js
@@ -1,10 +1,20 @@
         <style>
+            * {
+                text-align: center;
+            }
             body {
+
                 border: 5px solid rgb(247, 208, 36);
                 box-shadow: 10px 10px 10px papayawhip;
             }
             .surveyContainer {
                 /* border: 5px solid hotpink; */
+                /* display: flex; */
+                text-align: center;
+                display: grid;
+                grid-template-rows: 1fr;
+                grid-template-columns: 1fr;
+                
                 transform: scale(0.3);
                 box-shadow: 12px 8px 15px rgb(247, 208, 36);
             }
@@ -37,6 +47,10 @@
 
             let containerHeight = surveyCont.height().toFixed()
             let containerWIdth = surveyCont.width().toFixed()
+
+            let h1 = $('h1')
+            h1.unbind('mouseenter') // this allows the h1 to be untargetable with a mouseevent. 
+            // section.append(h1) && h1.mouseenter() is being targeted with the same event governed by parent container events.
 
              
             let strainmap = new Map()
@@ -74,31 +88,58 @@
 
                 let ajaxstrain = ajdata.strains
 
-                const findEffect = () => {
+                const findEffect = (elem) => {
                     let randomstrain = ajaxstrain[Math.floor(Math.random()*ajaxstrain.length)]
-                    console.log('randomstrain and name')
-                    console.log(randomstrain)
-                    console.log(randomstrain.strain)
-                    strainmap.set('strain', `${randomstrain.gold}`)
+                    let randomname = randomstrain.strain
+                    let randomgold = randomstrain.gold
+                    let prepoint = randomgold.split(', ')
+                    let wordlength = prepoint.length -1 // factoring in arraybased index making sure none of our values are exclusive to our options created with our randomizers.
+                    let randomword = prepoint[Math.floor(Math.random()*prepoint.length)]
+
+                    let randomwordptag = document.createElement('p')
+                    $(randomwordptag)
+                    .text(randomword)
+                    .css("font-size", "100px")      //wonder why this has to be so large.
+                    .css("color", "papayawhip")
+                    // .addClass("papaya")
+                    $(event.target).append()
+                    $(elem).append($(randomwordptag))
+                    // section.append($(randomwordptag))
+
+                    strainmap.set(randomname, randomword)
+                     randomstrain = ''
+                     randomname = ''
+                     randomgold = ''
+                     prepoint = ''
+                     wordlength = ''
+                     randomword = ''
+                    console.log('randomname')
+                    console.log(randomname)  
+                    console.log('strainmap')                              
+                    console.log(strainmap)                              
+                    // strainmap.set('strain', `${randomstrain.gold}`)
                 }   
+                // findEffect()
+
+            
 
                 section
                 .mouseenter( (event) => {
                     $(event.target).css('border', '10px solid papayawhip')
                     console.log('thats called hovering')
-                    findEffect()
-                    let randomStrain = strainmap.get('strain')
-                    console.log('randomStrain')
-                    console.log(randomStrain)
-                    // let prepainpoint = randomStrain.split()
-                    let prepainpoint = randomStrain.split(', ')  // (2) ['insomnia', 'focused'] ** this is what we want ** picking one word from db.strain.gold
-                    console.log('prepainpoint')
-                    console.log(prepainpoint)
+                    findEffect($(event.target))
                     $(event.target).unbind('mouseenter')
-                    
                 })
-
-
+                    
+                // let randomStrain = strainmap.get('strain')
+                // console.log('randomStrain')
+                // console.log(randomStrain)
+                // // let prepainpoint = randomStrain.split()
+                // let prepainpoint = randomStrain.split(', ')  // (2) ['insomnia', 'focused'] ** this is what we want ** picking one word from db.strain.gold
+                // console.log('prepainpoint')
+                // console.log(prepainpoint)
+                
+                
 
 
             })
